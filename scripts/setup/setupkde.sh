506374445153d35580692cdc8e45fb7ab379e53f
avivilloz <avivilloz@gmail.com>
2022-05-26 15:37:36 +0300

    ur

--- scripts/setup/setupkde.sh
+++ scripts/setup/setupkde.sh
@@ -4,10 +4,16 @@ echo -------------------------------------------------------------------------
 echo setup kde
 echo -------------------------------------------------------------------------
 
-echo do you want to setup kde? [y/n]
+echo do you want to do a complete install of kde on arch - includes fonts installation, and needs yay installed? [y/n]
 read ans
 if [ $ans  = "y" ]; then
-    sudo pacman -S plasma sddm kdeconnect ark dolphin konsole gwenview kronometer qalculate-gtk okular --needed
-    yay -S nerd-fonts-complete kwin-bismuth --needed
-    bash ~/git/utils/scripts/sync/synckde.sh -d gtl
+    sudo pacman -S plasma sddm kdeconnect ark dolphin konsole gwenview okular spectacle kcolorchooser --neAeded
+    yay -S nerd-fonts-complete kwin-bismuth kclock --needed
+fi
+
+echo do you want to setup kde with config files, etc? [y/n]
+read ans
+if [ $ans  = "y" ]; then
+    . ~/.variables
+    bash $scriptsdir/sync/synckde.sh -d gtl
 fi
