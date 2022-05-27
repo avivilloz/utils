2e26d57870e8cc0b64578e07e9b28ff8600b3214
avivilloz <avivilloz@gmail.com>
2022-05-27 17:41:49 +0300

    ur

--- scripts/mpd/mm.sh
+++ scripts/mpd/mm.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-if [ "$(mpc playlist)" = "" ]
-then
-    mpc update && mpc ls | mpc add 
+#if [ "$(mpc playlist)" = "" ]
+#then
+#    mpc update && mpc ls | mpc add 
+#fi
+
+mmstatusfile=~/.mmstatus
+mmstatus=$(head -n 1 $mmstatusfile)
+if [ "$mmstatus" = "opened" ]; then
+    exit 0
 fi
+
+echo "opened" > ~/.mmstatus
+mpc clear
+mpc update && mpc ls | mpc add 
+trap 'mpc clear && echo closed > ~/.mmstatus' SIGHUP
+trap 'mpc clear && echo closed > ~/.mmstatus' INT
 ncmpcpp
+mpc clear && echo "closed" > $mmstatusfile
