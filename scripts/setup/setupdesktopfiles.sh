481cee84268157cf7f5a5469e7283c5647bd3f3f
avivilloz <avivilloz@gmail.com>
2022-05-30 00:49:52 +0300

    ur

--- scripts/setup/setupdesktopfiles.sh
+++ scripts/setup/setupdesktopfiles.sh
@@ -7,7 +7,7 @@ echo -------------------------------------------------------------------------
 echo do you want to setup desktop files? [y/n]
 read ans
 if [ $ans  = "y" ]; then
-	mkdir -p ~/local/share/applications
-	rm -rf ~/local/share/applications/df
+	mkdir -p ~/.local/share/applications
+	rm -rf ~/.local/share/applications/df
 	ln -sf ~/git/utils/dotfiles/df ~/.local/share/applications/df
 fi
