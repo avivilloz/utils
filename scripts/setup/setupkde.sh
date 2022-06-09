5eb313793e8ba2e46020950617276a52a038f6de
avivilloz <avivilloz@gmail.com>
2022-06-09 15:35:03 +0300

    ur

--- scripts/setup/setupkde.sh
+++ scripts/setup/setupkde.sh
@@ -11,16 +11,12 @@ if [ $ans  = "y" ]; then
     yay -S nerd-fonts-complete kwin-bismuth kclock --needed
 fi
 
-echo do you want to install needed packages for kde-neon? [y/n]
+echo do you want to install needed packages for debian? [y/n]
 read ans
 if [ $ans  = "y" ]; then
-	pkcon install kwin-bismuth
-fi
-
-echo do you want to install needed packages for ubuntu? [y/n]
-read ans
-if [ $ans  = "y" ]; then
-	sudo apt install kwin-bismuth
+	echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list > /dev/null
+	wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
+	sudo apt update && sudo apt install kwin-bismuth
 fi
 
 echo do you want to config kde? [y/n]
