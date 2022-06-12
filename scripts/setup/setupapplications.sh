baa5ba6edc810498b1652c32bed37ac3b7d006ad
avivilloz <avivilloz@gmail.com>
2022-06-12 05:02:34 +0300

    ur

--- scripts/setup/setupapplications.sh
+++ scripts/setup/setupapplications.sh
@@ -7,42 +7,44 @@ echo -------------------------------------------------------------------------
 echo do you want to ? [y/n]
 read ans
 if [ $ans  = "y" ]; then
-	sudo pacman -S obsidian
-	sudo pacman -S bitwarden
-	sudo pacman -S blender
-	sudo pacman -S audacity
-	sudo pacman -S discord
-	sudo pacman -S flameshot
-	sudo pacman -S gcompris-qt
-	sudo pacman -S balena-etcher
-	sudo pacman -S github-desktop
-	sudo pacman -S firefox-appmenu
-	sudo pacman -S gimp
-	sudo pacman -S gnucash
-	sudo pacman -S godot
-	sudo pacman -S htop
-	sudo pacman -S inkscape
-	sudo pacman -S libreoffice-fresh
-	sudo pacman -S mpv
-	sudo pacman -S vlc
-	sudo pacman -S obs-studio
-	sudo pacman -S virtualbox virtualbox-host-modules-arch
-	sudo pacman -S qalculate-qt
-	sudo pacman -S ranger
-	sudo pacman -S timeshift
-	sudo pacman -S telegram-qt
-	sudo pacman -S stacer
-	sudo pacman -S kdenlive
-	sudo pacman -S krita
-	sudo pacman -S kcharselect
-	sudo pacman -S kcolorchooser
-	sudo pacman -S ktorrent
-	sudo pacman -S sweeper
-	sudo pacman -S steam
-	sudo pacman -S visual-studio-code-bin
-	sudo pacman -S wireshark-qt
-	sudo pacman -S zoom
-	sudo pacman -S stremio
-	sudo pacman -S akregator
-	yay -S aseprite
+	sudo pacman -S vim --needed
+	sudo pacman -S git --needed
+	sudo pacman -S obsidian --needed
+	sudo pacman -S bitwarden --needed
+	sudo pacman -S blender --needed
+	sudo pacman -S audacity --needed
+	sudo pacman -S discord --needed
+	sudo pacman -S flameshot --needed
+	sudo pacman -S gcompris-qt --needed
+	sudo pacman -S balena-etcher --needed
+	sudo pacman -S github-desktop --needed
+	sudo pacman -S firefox-appmenu --needed
+	sudo pacman -S gimp --needed
+	sudo pacman -S gnucash --needed
+	sudo pacman -S godot --needed
+	sudo pacman -S htop --needed
+	sudo pacman -S inkscape --needed
+	sudo pacman -S libreoffice-fresh --needed
+	sudo pacman -S mpv --needed
+	sudo pacman -S vlc --needed
+	sudo pacman -S obs-studio --needed
+	sudo pacman -S virtualbox virtualbox-host-modules-arch --needed
+	sudo pacman -S qalculate-qt --needed
+	sudo pacman -S ranger --needed
+	sudo pacman -S timeshift --needed
+	sudo pacman -S telegram-qt --needed
+	sudo pacman -S stacer --needed
+	sudo pacman -S kdenlive --needed
+	sudo pacman -S krita --needed
+	sudo pacman -S kcharselect --needed
+	sudo pacman -S kcolorchooser --needed
+	sudo pacman -S ktorrent --needed
+	sudo pacman -S sweeper --needed
+	sudo pacman -S steam --needed
+	sudo pacman -S visual-studio-code-bin --needed
+	sudo pacman -S wireshark-qt --needed
+	sudo pacman -S zoom --needed
+	sudo pacman -S stremio --needed
+	sudo pacman -S akregator --needed
+	yay -S aseprite --needed
 fi
