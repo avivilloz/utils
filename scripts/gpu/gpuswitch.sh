a0661527b254139a5a71f5d6b0c984196f7bde66
avivilloz <avivilloz@gmail.com>
2022-06-13 22:36:46 +0300

    ur

--- scripts/gpu/gpuswitch.sh
+++ scripts/gpu/gpuswitch.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-sudo envycontrol --dm sddm -s $1
+envycontrol --dm sddm -s $1
 
-echo "intern=eDP-1-1
+echo "
+intern=eDP-1-1
 mon1=HDMI-0
 
-if xrandr | grep "$mon1 connected"; then
-    xrandr --output $intern --off
-    xrandr --output $mon1 --mode 1920x1080 --pos 0x0 --rotate normal
+if [ xrandr | grep \"\$mon1 connected\" ]; then
+    xrandr --output \$intern --off
+    xrandr --output \$mon1 --mode 1920x1080 --pos 0x0 --rotate normal
 else
-    xrandr --output $intern --mode 1920x1080 --pos 0x0 --rotate normal 
+    xrandr --output \$intern --mode 1920x1080 --pos 0x0 --rotate normal 
 fi" >> /usr/share/sddm/scripts/Xsetup
