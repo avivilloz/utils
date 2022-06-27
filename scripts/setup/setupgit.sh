ce0f29ddce6a2543c12d2e797a6d8f52967424b0
avivilloz <avivilloz@gmail.com>
2022-06-27 12:02:17 +0300

    ur

--- scripts/setup/setupgit.sh
+++ scripts/setup/setupgit.sh
@@ -8,12 +8,7 @@ echo do you want to setup git? [y/n]
 read ans
 if [ $ans  = "y" ]; then
     . ~/.variables
-
-	mkdir -p ~/.ssh
-	cd ~/.ssh
-	cp -rf ~/git/utils/dotfiles/ssh/config .
-
 	for key in ${!git_users[@]}; do
-		ssh-keygen -t ed25519 -C "${git_users[${key}]}" -f "id_ed25519_${key}"
+		bash ~/git/utils/scripts/git/git_ssh_keygen.sh ${key} ${git_users[${key}]}
 	done
 fi
