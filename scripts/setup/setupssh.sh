15713ae04f00695b34668bbf2b8765fde2807534
avivilloz <avivilloz@gmail.com>
2022-06-27 07:47:23 +0300

    ur

--- scripts/setup/setupssh.sh
+++ scripts/setup/setupssh.sh
@@ -7,6 +7,9 @@ echo -------------------------------------------------------------------------
 echo do you want to setup ssh? [y/n]
 read ans
 if [ $ans  = "y" ]; then
-	rm -rf ~/.ssh
-	ln -sf ~/git/utils/dotfiles/ssh/.ssh ~/.ssh
+	. ~/.variables
+	mkdir -p ~/.ssh
+	cd ~/.ssh
+	cp ~/git/utils/dotfiles/ssh/.ssh/config .
+	ssh-keygen -t ed25519 -C "$git_user_email" -f id_ed25519_avivilloz
 fi
