5d7cdd4cc932f452de191efc93480d302c531e0e
avivilloz <avivilloz@gmail.com>
2022-07-28 18:00:32 +0300

    ur

---
+++ scripts/setup/setup_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+bash ~/git/utils/scripts/setup/setup_wifi.sh
+bash ~/git/utils/scripts/setup/setup_bash.sh
+bash ~/git/utils/scripts/setup/setup_vim.sh
+bash ~/git/utils/scripts/setup/setup_git.sh
+bash ~/git/utils/scripts/setup/setup_sda.sh
+bash ~/git/utils/scripts/setup/setup_packagemanagers.sh
+bash ~/git/utils/scripts/setup/setup_kde.sh
+bash ~/git/utils/scripts/setup/setup_desktopfiles.sh
+bash ~/git/utils/scripts/setup/setup_applications.sh
+bash ~/git/utils/scripts/setup/setup_obs.sh
