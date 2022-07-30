ff47fbed88db92d4a04df6c6d6581d67eb7e0cb0
avivilloz <avivilloz@gmail.com>
2022-07-30 21:55:50 +0300

    ur

--- scripts/setup/setup_fs.sh
+++ scripts/setup/setup_fs.sh
@@ -9,61 +9,65 @@ read ans
 if [ $ans  = "y" ]; then
 	echo "username:"
 	read username
-	real_user="/home/$username"
-	utils_fs_dir="$real_user/git/utils/fs"
+	real_home="/home/$username"
+	utils_fs_dir="$real_home/git/utils/fs"
 
 	etc="$utils_fs_dir/etc"
-	for file in $etc/*; do
-		if [ -f $file ]; then
-			link=${file#"$utils_fs_dir"}
-			rm -rf $link
-			ln -sf $file $link
-		fi
-	done
+	if [ -f $etc ]; then
+		for file in $etc/*; do
+			if [ -f $file ]; then
+				link=${file#"$utils_fs_dir"}
+				rm -rf $link
+				ln -sf $file $link
+			fi
+		done
+	fi
 
 	usr_share="$utils_fs_dir/usr/share"
-	for file in $usr_share/*; do
-		if [ -f $file ] || [ -d $file ]; then
-			link=${file#"$utils_fs_dir"}
-			rm -rf $link
-			ln -sf $file $link
-		fi
-	done
+	if [ -f $usr_share ]; then
+		for file in $usr_share/*; do
+			if [ -f $file ] || [ -d $file ]; then
+				link=${file#"$utils_fs_dir"}
+				rm -rf $link
+				ln -sf $file $link
+			fi
+		done
+	fi
 
-	#only files in home/user
-	user="$utils_fs_dir/home/user"
-	for file in $user/* $user/.[^.]*; do
+	#only files in home
+	home="$utils_fs_dir/home"
+	for file in $home/* $home/.[^.]*; do
 		if [ -f $file ]; then
-			user_link="$real_user${file#"$user"}"
-			root_link="/root${file#"$user"}"
-			rm -rf $user_link
-			ln -sf $file $user_link
+			home_link="$real_home${file#"$home"}"
+			root_link="/root${file#"$home"}"
+			rm -rf $home_link
+			ln -sf $file $home_link
 			rm -rf $root_link
 			ln -sf $file $root_link
 		fi
 	done
 
-	#directories in home/user
-	rm -rf $real_user/.fonts
-	ln -sf $user/.fonts $real_user/.fonts
+	#directories in home
+	rm -rf $real_home/.fonts
+	ln -sf $home/.fonts $real_home/.fonts
 
-	user_config="$user/.config"
-	real_user_config="$real_user/.config"
-    mkdir -p $real_user_config
-	for file in $user_config/*; do
+	home_config="$home/.config"
+	real_home_config="$real_home/.config"
+	mkdir -p $real_home_config
+	for file in $home_config/*; do
 		if [ -f $file ] || [ -d $file ]; then
-			link="$real_user_config${file#"$user_config"}"
+			link="$real_home_config${file#"$home_config"}"
 			rm -rf $link
 			ln -sf $file $link
 		fi
 	done
 
-	user_local_share="$user/.local/share"
-	real_user_local_share="$real_user/.local/share"
-    mkdir -p $real_user_local_share
-	for file in $user_local_share/*; do
+	home_local_share="$home/.local/share"
+	real_home_local_share="$real_home/.local/share"
+	mkdir -p $real_home_local_share
+	for file in $home_local_share/*; do
 		if [ -f $file ] || [ -d $file ]; then
-			link="$real_user_local_share${file#"$user_local_share"}"
+			link="$real_home_local_share${file#"$home_local_share"}"
 			rm -rf $link
 			ln -sf $file $link
 		fi
