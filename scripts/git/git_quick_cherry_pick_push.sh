518f8cd8ed6eb5c5891164002e04399ccc8901ee
avivilloz <avivilloz@gmail.com>
2022-08-29 09:10:19 +0300

    ur

--- scripts/git/git_quick_cherry_pick_push.sh
+++ scripts/git/git_quick_cherry_pick_push.sh
@@ -14,13 +14,14 @@ git_user=$1
 main_branch=$2
 new_branch_purpose=$3
 commit=$4
+should_push=$5
 
 new_branch=$git_user/$new_branch_purpose\_$main_branch
 
 git checkout $main_branch
 git pull
 git checkout -b $new_branch
-git cherry-pick $commit
+git cherry-pick --strategy=recursive -X theirs $commit
 git push --set-upstream origin $new_branch
 
 exit 0
---
+++ scripts/guardicore/edit_tester_annotations.py
@@ -0,0 +1,18 @@
+#import requests
+#from tests.qa.jenkins.utils.tester_configs import AOL_CONF
+
+import sys
+sys.path.append('/home/ailloz/git/guardicore/tests/qa/jenkins/utils/')
+from tester_configs import AOL_CONF
+from copy import deepcopy
+
+annotations = deepcopy(AOL_CONF)
+annotations.update({
+    'type': 'thin',
+    'environment_name': 'Thin-5-Regular',
+    'vcenter': '10.0.0.22',
+    'build_nightly': True
+})
+
+res = requests.post('https://tester-config.gc.guardicore.com/save_details', data=annotations,
+                    verify=False)
