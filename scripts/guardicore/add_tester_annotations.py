fbdd6f86602d81f9fd92b2476dc3bf96c3cebf9e
avivilloz <avivilloz@gmail.com>
2022-11-03 14:34:49 +0200

    ur

--- scripts/guardicore/add_tester_annotations.py
+++ scripts/guardicore/add_tester_annotations.py
@@ -11,7 +11,7 @@ from copy import deepcopy
 annotations = deepcopy(AOL_CONF)
 annotations.update({
     'type': 'thin',
-    'environment_name': 'Thin-293-Regular',
+    'environment_name': 'Thin-300-Regular',
     'vcenter': '10.0.0.22',
     'build_nightly': True
 })
