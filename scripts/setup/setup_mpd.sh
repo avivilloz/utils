b05a3b5d4a1f81b04e94123287d56adef3af2fcd
avivilloz <avivilloz@gmail.com>
2022-11-25 12:08:24 +0200

    ur

--- scripts/setup/setup_mpd.sh
+++ scripts/setup/setup_mpd.sh
@@ -10,8 +10,4 @@ if [ $ans  = "y" ]; then
 	mkdir -p ~/.config/mpd
 	ln -sf ~/git/utils/dotfiles/mpd/mpd.conf ~/.config/mpd/mpd.conf
 	systemctl --user enable mpd
-
-	mkdir -p ~/.local/share/applications
-	ln -sf ~/git/utils/assets/applications/music_manager.desktop \
-		~/.local/share/applications/music_manager.desktop
 fi
