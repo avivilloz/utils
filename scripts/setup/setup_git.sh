20f93f8b45b7dc6fb6b5e568d1b463f82cc3fadb
avivilloz <avivilloz@gmail.com>
2022-11-26 21:02:35 +0200

    ur

--- scripts/setup/setup_git.sh
+++ scripts/setup/setup_git.sh
@@ -9,6 +9,8 @@ read ans
 if [ $ans  = "y" ]; then
 	vim ~/.variables
 	. ~/.variables
+	git config --global user.name $git_name
+	git config --global user.email $git_email
 	for key in ${!git_users[@]}; do
 		bash ~/git/utils/scripts/git/git_ssh_keygen.sh ${key} ${git_users[${key}]}
 	done
