301f8e93ca9f3f193d577569c617a5a67b36fb7f
avivilloz <avivilloz@gmail.com>
2022-11-29 12:23:19 +0200

    ur

--- scripts/gpu/gpus.sh
+++ scripts/gpu/gpus.sh
@@ -4,16 +4,30 @@
 
 if [ "$base_distro" = "arch" ];then
 
-    sudo envycontrol --dm $display_manager -s $1
+    if [ "$1" = "nvidia" ]; then
+        sudo envycontrol --dm $display_manager -s $1
+        sudo sed -i 's/#WaylandEnable/WaylandEnable/g' /etc/gdm/custom.conf > /dev/null
+    else
+        if ! grep -Fq "#WaylandEnable=false" /etc/gdm/custom.conf; then
+            sudo sed -i 's/WaylandEnable/#WaylandEnable/g' /etc/gdm/custom.conf > /dev/null
+        fi
+    fi
 
 elif [ "$base_distro" = "fedora" ];then
 
     if [ "$1" = "nvidia" ]; then
         sudo sed -i 's/#Option "PrimaryGPU" "yes"/Option "PrimaryGPU" "yes"/g' /etc/X11/xorg.conf.d/nvidia.conf > /dev/null
+        sudo sed -i 's/#WaylandEnable/WaylandEnable/g' /etc/gdm/custom.conf > /dev/null
     elif [ "$1" = "hybrid" ]; then
+
         if ! grep -Fq '#Option "PrimaryGPU" "yes"' /etc/X11/xorg.conf.d/nvidia.conf; then
             sudo sed -i 's/Option "PrimaryGPU" "yes"/#Option "PrimaryGPU" "yes"/g' /etc/X11/xorg.conf.d/nvidia.conf > /dev/null
         fi
+
+        if ! grep -Fq "#WaylandEnable=false" /etc/gdm/custom.conf; then
+            sudo sed -i 's/WaylandEnable/#WaylandEnable/g' /etc/gdm/custom.conf > /dev/null
+        fi
+
     fi
 
 fi
