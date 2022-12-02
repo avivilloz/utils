ac71f9ecc4c253fab6e492814c7bfd51a2a7a42e
avivilloz <avivilloz@gmail.com>
2022-12-02 12:40:44 +0200

    ur

--- scripts/gpu/gpus.sh
+++ scripts/gpu/gpus.sh
@@ -5,7 +5,6 @@
 if [ "$base_distro" = "arch" ];then
 
     if [ "$1" = "nvidia" ]; then
-        sudo envycontrol --dm $display_manager -s $1
         sudo sed -i 's/#WaylandEnable/WaylandEnable/g' /etc/gdm/custom.conf > /dev/null
     else
         if ! grep -Fq "#WaylandEnable=false" /etc/gdm/custom.conf; then
@@ -13,13 +12,29 @@ if [ "$base_distro" = "arch" ];then
         fi
     fi
 
+    sudo envycontrol --dm $display_manager -s $1
+
 elif [ "$base_distro" = "fedora" ];then
 
     if [ "$1" = "nvidia" ]; then
         sudo sed -i 's/#Option "PrimaryGPU" "yes"/Option "PrimaryGPU" "yes"/g' /etc/X11/xorg.conf.d/nvidia.conf > /dev/null
         sudo sed -i 's/#WaylandEnable/WaylandEnable/g' /etc/gdm/custom.conf > /dev/null
         sudo grubby --update-kernel=ALL --args='rd.driver.blacklist=nouveau modprobe.blacklist=nouveau nvidia-drm.modeset=1'
-    else
+    else if [ "$1" = "hybrid" ]; then
+
+        if ! grep -Fq '#Option "PrimaryGPU" "yes"' /etc/X11/xorg.conf.d/nvidia.conf; then
+            sudo sed -i 's/Option "PrimaryGPU" "yes"/#Option "PrimaryGPU" "yes"/g' /etc/X11/xorg.conf.d/nvidia.conf > /dev/null
+        fi
+
+        if ! grep -Fq "#WaylandEnable=false" /etc/gdm/custom.conf; then
+            sudo sed -i 's/WaylandEnable/#WaylandEnable/g' /etc/gdm/custom.conf > /dev/null
+        fi
+        
+        if ! grep -Fq "rd.driver.blacklist=nouveau modprobe.blacklist=nouveau nvidia-drm.modeset=1" /etc/default/grub; then
+            sudo grubby --update-kernel=ALL --args='rd.driver.blacklist=nouveau modprobe.blacklist=nouveau nvidia-drm.modeset=1'
+        fi
+
+    else if [ "$1" = "integrated" ]; then
 
         if ! grep -Fq '#Option "PrimaryGPU" "yes"' /etc/X11/xorg.conf.d/nvidia.conf; then
             sudo sed -i 's/Option "PrimaryGPU" "yes"/#Option "PrimaryGPU" "yes"/g' /etc/X11/xorg.conf.d/nvidia.conf > /dev/null
