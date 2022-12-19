74a6903b7477c1f1d72017e11c172f07659dc0ff
avivilloz <avivilloz@gmail.com>
2022-12-19 13:26:40 +0200

    ur

--- scripts/guardicore/add_tester_annotations.py
+++ scripts/guardicore/add_tester_annotations.py
@@ -4,18 +4,18 @@
 
 import requests
 import sys
-sys.path.append('/home/ailloz/git/guardicore/tests/qa/jenkins/utils/')
+sys.path.append('/home/ailloz/git/guardicore2/tests/qa/jenkins/utils/')
 from tester_configs import AOL_CONF
 from copy import deepcopy
 
 annotations = deepcopy(AOL_CONF)
 annotations.update({
     'type': 'thin',
-    'environment_name': 'Thin-292-Regular',
+    'environment_name': 'Thin-293-Regular',
     'vcenter': '10.0.0.22',
     'build_nightly': True,
-    'testing_branch': 'v42.1.8-f4.0-221120.408fa0a.198',
-    'branch': 'v42.1.8-f4.0-221120.408fa0a.198'
+    'testing_branch': 'v42',
+    'branch': 'v42'
 })
 
 res = requests.post('https://tester-config.gc.guardicore.com/save_details', data=annotations,
