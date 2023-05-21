ad5a18cdfca9cc58d7a6ca4921cf64be76ceae18
avivilloz <avivilloz@gmail.com>
2023-05-21 17:40:51 +0300

    ur

--- scripts/guardicore/add_tester_annotations.py
+++ scripts/guardicore/add_tester_annotations.py
@@ -1,17 +1,17 @@
 #EDIT BEFORE IT RUNS
 
-#from tests.qa.jenkins.utils.tester_configs import AOL_CONF
+#from tests.qa.jenkins.utils.tester_configs import SPAN_COLLECTOR
 
 import requests
 import sys
 sys.path.append('/home/ailloz/git/guardicore2/tests/qa/jenkins/utils/')
-from tester_configs import AOL_CONF
+from tester_configs import SPAN_COLLECTOR
 from copy import deepcopy
 
-annotations = deepcopy(AOL_CONF)
+annotations = deepcopy(SPAN_COLLECTOR)
 annotations.update({
     'type': 'thin',
-    'environment_name': 'Thin-124-Regular',
+    'environment_name': 'Thin-323-Regular',
     'vcenter': '10.0.0.22',
     'build_nightly': False,
     'testing_branch': 'master',
