ea9e6efcf2820e2d33bf7777abc5b3cc1d8dd2d9
avivilloz <avivilloz@gmail.com>
2023-08-10 09:49:32 +0300

    ur

--- scripts/setup/deck/setup.sh
+++ scripts/setup/deck/setup.sh
@@ -23,5 +23,8 @@ sudo cp /home/deck/git/utils/dotfiles/pacman/pacman.conf /etc
 bash ~/git/utils/scripts/setup/deck/setup_package_managers.sh
 bash ~/git/utils/scripts/setup/deck/setup_applications.sh
 
+sudo pacman -S fakeroot
+
 kwriteconfig5 --file kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.krunner,/App,,toggleDisplay"
 kwin_x11 --replace
+
