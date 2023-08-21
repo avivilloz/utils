e59d647b5c04edff34d585deb6e54a2b6ba946ec
avivilloz <avivilloz@gmail.com>
2023-08-21 12:52:33 +0300

    ur

--- scripts/godot/format_gdscript.sh
+++ scripts/godot/format_gdscript.sh
@@ -7,6 +7,6 @@ regex='selected_script="res://([^"]*)"'
 script_local_path=$(grep -Po "$regex" "$editor_layout_path" | sed -nE 's|'"$regex"'|\1|p')
 script_global_path="$godot_project_path/$script_local_path"
 
-echo "Script to be formatted: $script_global_path" >> ~/hello
+echo "Script to be formatted: $script_global_path"
 
 gdformat $script_global_path
