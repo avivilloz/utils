286505ae3915f587f50e72a62107b16ceccb4c62
avivilloz <avivilloz@gmail.com>
2024-05-27 16:34:51 +0300

    ur

--- scripts/pkg/pkg.sh
+++ scripts/pkg/pkg.sh
@@ -77,73 +77,24 @@ elif [ "$base_distro" == "arch" ]; then
         for pkg in "$@"; do
             if [ "$pkg" != "$1" ]; then
                 yay -S --needed $2
-                if  [[ ! $(yay -Qe | grep -i $pkg) ]]; then
-                    flatpak install $pkg
-                fi
             fi
         done
     elif [ "$1" == "rm" ]; then
         for pkg in "$@"; do
             if [ "$pkg" != "$1" ]; then
                 yay -Rns $2
-                flatpak uninstall $2
             fi
         done
     elif [ "$1" == "up" ]; then
         yay -Syu
         yay -Yc
         yay -Scc
-        flatpak update
-        flatpak uninstall --unused
     elif [ "$1" == "l" ]; then
         yay -Qe
-        flatpak list
     elif [ "$1" == "lg" ]; then
         yay -Qe | grep -i $2
-        flatpak list --app | grep -i $2
     elif [ "$1" == "q" ]; then
         yay -Ss $2
-    fi
-
-elif [ "$base_distro" == "deck" ]; then
-    if [ "$1" == "r" ]; then
-        sudo pacman -Syy
-    elif [ "$1" == "ss" ]; then
-        sudo pacman -Syy
-        sudo pacman -Sua
-    elif [ "$1" == "u" ]; then
-        sudo pacman -S $2
-    elif [ "$1" == "in" ]; then
-        sudo pacman -Syy
-        for pkg in "$@"; do
-            if [ "$pkg" != "$1" ]; then
-                sudo pacman -S --needed $2
-                if  [[ ! $(sudo pacman -Qe | grep -i $pkg) ]]; then
-                    flatpak install $pkg
-                fi
-            fi
-        done
-    elif [ "$1" == "rm" ]; then
-        for pkg in "$@"; do
-            if [ "$pkg" != "$1" ]; then
-                sudo pacman -Rns $2
-                flatpak uninstall $2
-            fi
-        done
-    elif [ "$1" == "up" ]; then
-        sudo pacman -Syu
-        sudo pacman -Yc
-        sudo pacman -Scc
-        flatpak update
-        flatpak uninstall --unused
-    elif [ "$1" == "l" ]; then
-        sudo pacman -Qe
-        flatpak list
-    elif [ "$1" == "lg" ]; then
-        sudo pacman -Qe | grep -i $2
-        flatpak list --app | grep -i $2
-    elif [ "$1" == "q" ]; then
-        sudo pacman -Ss $2
     fi
 
 fi
